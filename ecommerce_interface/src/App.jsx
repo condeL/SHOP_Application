fe7034f52f15f1d2946a0ace84422e184095d656
Lanciné Condé <l.conde@student.lancaster.edu.gh>
2022-04-12 21:01:49 +0000

    Added scroll to top

--- ecommerce_interface/src/App.jsx
+++ ecommerce_interface/src/App.jsx
@@ -7,26 +7,37 @@ import Cart from "./pages/Cart"
 import {
     BrowserRouter as Router, Navigate,
     Route,
-    Routes
+    Routes, useLocation
 } from "react-router-dom";
 import {useSelector} from "react-redux";
 import NotFound from "./pages/NotFound";
+import {useLayoutEffect} from "react";
 
+
+const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+        document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+}
 
 const App = () => {
     const user = useSelector(state => state.user.currentUser);
     return (
         <Router>
-            <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route path="/products/" element={<ProductList/>}/>
-                <Route path="/products/category/:category" element={<ProductList/>}/>
-                <Route path="/product/:id" element={<Product/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/register" element={user? <Navigate to="/"/> : <Register/>}/>
-                <Route path="/login" element={user? <Navigate to="/"/> : <Login/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
+            <Wrapper>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path="/products/" element={<ProductList/>}/>
+                    <Route path="/products/category/:category" element={<ProductList/>}/>
+                    <Route path="/product/:id" element={<Product/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/register" element={user? <Navigate to="/"/> : <Register/>}/>
+                    <Route path="/login" element={user? <Navigate to="/"/> : <Login/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Wrapper>
         </Router>
     )
 }
